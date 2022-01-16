5b1a653824b146f026ae30f5bf9b485ec2508335
Adam Rauch <adam@labkey.com>
2022-01-16 12:16:56 -0800

    Consolidate bootstrap scripts to 0.000-21.000.sql (#62)
    Co-authored-by: labkey-tchad <tchad@labkey.com>

--- resources/schemas/dbscripts/postgresql/mobileappstudy-20.000-20.001.sql
+++
@@ -1,4 +0,0 @@
--- Add new column and populate existing rows with INVALID
-ALTER TABLE mobileappstudy.Participant ADD COLUMN AllowDataSharing VARCHAR(20) NOT NULL DEFAULT 'INVALID';
--- No longer need that default constraint; insert must supply this value
-ALTER TABLE mobileappstudy.Participant ALTER COLUMN AllowDataSharing DROP DEFAULT;
